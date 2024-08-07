f39b721374bf734ce7e555b4c2cf384bf97284db
abdul <abdul.tanveer@gmail.com>
2024-08-07 10:49:41 +0530

    comment

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
 
     <Routes>
+      {/* add two more pages */}
       <Route path='/' element = {<Home/>}></Route>
       <Route path='/about' element = {<About/>}></Route>
 
